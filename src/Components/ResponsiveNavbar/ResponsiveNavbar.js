f0457dfc9fd20590a0e1d88ac61ba9abc6ae1c57
Apitchaya Radchakit <apitchaya.rchk@gmail.com>
2022-04-21 12:22:17 +0700

    edit logo for ios

--- src/Components/ResponsiveNavbar/ResponsiveNavbar.js
+++ src/Components/ResponsiveNavbar/ResponsiveNavbar.js
@@ -12,25 +12,13 @@ export default function ResponsiveNavbar() {
         <div className='flex justify-center shadow-md'>
             <div className="menuBar flex justify-between w-full md:w-5/6 p-3">
                 <div className="brandSection flex flex-col justify-center items-center ">
-                    <img src={logo} alt='Logo' className="h-7 w-fit object-cover" />
+                    <img src={logo} alt='Logo' className="h-7 w-auto md:w-fit object-cover" />
                     <div className="text-sm md:text-base">Infinity Activity</div>
-
                 </div>
                 <ul className="navSection flex space-x-4 items-center ">
-            
-                    {/* <li className=""><a href="#">Home</a></li> */}
                     <li className="text-sm md:text-base"><Link to="/addjourney">New Journey</Link></li>
                     <li className="text-sm md:text-base"><Link to="/dashboard">Journey</Link></li>
-                    {/* <li className=""><a href="#">Settings</a></li> */}
                 </ul>
-                {/* <div className='loginSection flex space-x-4 items-center'>
-                    <button className="bg-gray-400 hover:bg-gray-600 text-white  py-2 px-4 rounded">
-                        Sign in
-                    </button>
-                    <button className="bg-gray-400 hover:bg-gray-600 text-white  py-2 px-4 rounded">
-                        Sign up
-                    </button>
-                </div> */}
             </div>
         </div>
     )
